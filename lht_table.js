2b2002e7aba5fc323d85de51b1be4d4ac18113df
Adrian <Asoliz241-west-mec.org>
2020-02-03 17:26:53 -0700

    comments

--- lht_table.js
+++ lht_table.js
@@ -10,7 +10,7 @@
 
 	
 */
-
+//setting the header of the schedule and starting the table
 var thisDay =  new Date("August 30, 2018");
 var tableHTML = "<table id='eventTable'>" ;
 tableHTML += "<caption> upcoming events</caption> ";
@@ -20,7 +20,7 @@ tableHTML += "<th>Events</th>";
 tableHTML += "<th>price</th> </tr>";
 tableHTML +="</tr>";
 var endDate = new Date(thisDay.getTime() + (14*24*60*60*1000));
-
+//loop for the days
 for(var i = 0 ; i < eventDates.length; i++){
    var eventDate = new Date(eventDates[i]);
    var eventDay =  eventDate.toDateString();
@@ -33,6 +33,7 @@ for(var i = 0 ; i < eventDates.length; i++){
       tableHTML+=  "<td>" +  eventPrices[i]  + "</td>";
       tableHTML+= "</tr>";
    }
-  
-} tableHTML += "</table>"
+} 
+//ending the table 
+tableHTML += "</table>"
    document.getElementById("eventList").innerHTML = tableHTML;
